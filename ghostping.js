0a9a25efa18904b1ec664f0c82dea9a97b1b635e
Grzechu439 <130742147+Grzechu439@users.noreply.github.com>
2023-10-15 11:16:22 +0200

    Update ghostping.js

--- ghostping.js
+++ ghostping.js
@@ -1,12 +1,13 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
 
-
-client.on('messageDelete', async (deletedMessage) => {
+module.exports = {
+  name: 'ghostping',
+  execute(deletedMessage) {
     if (deletedMessage.mentions.members.first()) {
       const mentionedUser = deletedMessage.mentions.members.first();
       const ghostPingMessage = `**Ghost Ping:** ${deletedMessage.author} spingował(a) ${mentionedUser} i usunął(a) wiadomość.`;
       
       deletedMessage.channel.send(ghostPingMessage);
     }
-  });
+  }
+};
